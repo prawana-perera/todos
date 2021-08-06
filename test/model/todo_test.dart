b8302606130bb398157d4438db690a09a6e92fc6
Prawana Perera <prawana.perera@gmail.com>
2021-08-06 16:08:48 +1000

    wip codecov path fix

--- test/model/todo_test.dart
+++ test/model/todo_test.dart
@@ -1,5 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:todos/model/todo.dart';
+import 'package:todos/main.dart';
+import 'package:todos/screens/tododetail.dart';
+import 'package:todos/screens/todolist.dart';
 
 void main() {
   group('toMap', () {
