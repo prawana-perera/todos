c2d00e36f591c044ee9e67b531abc0c947d3b7db
Prawana Perera <prawana.perera@gmail.com>
2021-08-06 16:15:03 +1000

    wip code coverage fixes

--- test/model/todo_test.dart
+++ test/model/todo_test.dart
@@ -1,8 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:todos/model/todo.dart';
-import 'package:todos/main.dart';
-import 'package:todos/screens/tododetail.dart';
-import 'package:todos/screens/todolist.dart';
 
 void main() {
   group('toMap', () {
