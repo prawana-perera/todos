3d95535c3f670a9a528557cbe99d36c843e8c7c3
Prawana Perera <prawana.perera@gmail.com>
2021-08-27 15:13:27 +1000

    added db tests and removed redundant model classes

--- lib/src/database/database.dart
+++ lib/src/database/database.dart
@@ -11,11 +11,17 @@ part 'database.g.dart';
 // Note: Table name plural, so that the generated data class is singular
 class Todos extends Table {
   IntColumn get id => integer().autoIncrement()();
+
   TextColumn get title => text().withLength(max: 32)();
+
   TextColumn get description => text().withLength(max: 1024).nullable()();
+
   IntColumn get priority => integer()();
+
   DateTimeColumn get createdAt => dateTime()();
+
   DateTimeColumn get updatedAt => dateTime()();
+
   DateTimeColumn get endDateTime => dateTime().nullable()();
 }
 
@@ -32,8 +38,11 @@ LazyDatabase _openConnection() {
 
 @UseMoor(tables: [Todos])
 class TodosDatabase extends _$TodosDatabase {
-// we tell the database where to store the data with this constructor
+  // we tell the database where to store the data with this constructor
   TodosDatabase() : super(_openConnection());
+
+  // Used for unit tests
+  TodosDatabase.test(QueryExecutor e) : super(e);
 
   // you should bump this number whenever you change or add a table definition. Migrations
   // are covered later in this readme.
@@ -41,7 +50,13 @@ class TodosDatabase extends _$TodosDatabase {
   int get schemaVersion => 1;
 
   Future<List<Todo>> getAllTodos() => select(todos).get();
+
+  Future<Todo?> getTodo(int id) =>
+      (select(todos)..where((t) => t.id.equals(id))).getSingleOrNull();
+
   Future addTodo(TodosCompanion todo) => into(todos).insert(todo);
+
   Future updateTodo(Todo todo) => update(todos).replace(todo);
+
   Future deleteTodo(Todo todo) => delete(todos).delete(todo);
 }
